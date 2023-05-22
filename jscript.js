7c800b3d7ac1f80c650c7ae11e9effb0a42c8cc6
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-22 19:57:24 -0400

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,4 +1,16 @@
-  fetch('https://bnqm19oa07.execute-api.us-east-1.amazonaws.com/default')
-.then((response => response.json()))
-.then((data) => {document.getElementById("visitor").innerHTML = data})
+document.getElementById("visitor").innerHTML = "error"; 
+
+try{
+  const response = fetch('https://bnqm19oa07.execute-api.us-east-1.amazonaws.com/default')
+   .then((response) => response.json())
+   .then((data) => {document.getElementById("visitor").innerHTML = data});
+  console.log(response.status());
+  console.log(response.json());
+  if (!response.ok){
+    throw new Error("Network response was not OK");
+  }
+} catch (error){
+  console.error("There has been a problem with your fetch operation:", error);
+}
+
 
