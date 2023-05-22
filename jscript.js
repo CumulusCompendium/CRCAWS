4264ea7482f04078b85d30897fd85324b0b01138
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-22 16:25:02 -0400

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,5 +1,4 @@
-<script>
   fetch('https://bnqm19oa07.execute-api.us-east-1.amazonaws.com/default')
 .then((response => response.json()))
 .then((data) => {document.getElementById("visitor").innerHTML = data})
-</script>
+
