621ac0d2a1c73fa1927273890049c21f353b3a01
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-23 15:49:04 -0400

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,7 +1,8 @@
 document.getElementById("visitor").innerHTML = "error"; 
  const response = fetch('https://h4j6pir80l.execute-api.us-east-1.amazonaws.com/REST'); 
- console.log(response.status);
- response.json().then((data) => {document.getElementById("visitor").innerHTML = data});
+ console.log(response.status + "code");
+ .then((response => response.json()))
+ .then((data) => {document.getElementById("visitor").innerHTML = data});
  console.log(data);
 
 
