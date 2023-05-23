71e0a46fd22dccb92224a4a3190ef764b0433b36
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-22 21:14:16 -0400

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,10 +1,10 @@
 document.getElementById("visitor").innerHTML = "error"; 
 
 try{
-  const response = fetch('https://bnqm19oa07.execute-api.us-east-1.amazonaws.com/default')
+  const response = fetch('https://cumuluscompendium.click/getVisitors')
    .then((response) => response.json())
    .then((data) => {document.getElementById("visitor").innerHTML = data});
-  console.log(response.status());
+  console.log(response.status);
   console.log(response.json());
   if (!response.ok){
     throw new Error("Network response was not OK");
