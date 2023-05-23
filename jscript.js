32c1b58ff3f5e13f279f7acc5fd9bb27ab27af03
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-23 12:11:00 -0400

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,14 +1,8 @@
 document.getElementById("visitor").innerHTML = "error"; 
+ const response = fetch('https://cumuluscompendium.click/getVisitors'); 
+ console.log(response.status);
+ response.json().then((data) => {document.getElementById("visitor").innerHTML = data});
+ console.log(data);
 
-try{
-  const response = fetch('https://cumuluscompendium.click/getVisitors');
-   if (response.ok){ 
-     console.log(response.status);
-    response.json().then((data) => {document.getElementById("visitor").innerHTML = data});
-     console.log(data);
-   }  
-} catch (error){
-  console.error("There has been a problem with your fetch operation:", error);
-  }
 
 
