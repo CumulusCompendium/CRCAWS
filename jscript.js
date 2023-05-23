f2901624eaaf322bf03d51243e560106035f764d
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-23 11:25:31 -0400

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,15 +1,15 @@
 document.getElementById("visitor").innerHTML = "error"; 
 
 try{
-  const response = fetch('https://cumuluscompendium.click/getVisitors')
-   .then((response) => response.json())
-   .then((data) => {document.getElementById("visitor").innerHTML = data});
-  console.log(response.status);
-  console.log(response.json());
-  if (!response.ok){
-    throw new Error("Network response was not OK");
+  const response = fetch('https://cumuluscompendium.click/getVisitors');
+   if (response.ok){ 
+    .then((response) => response.json())
+    .then((data) => {document.getElementById("visitor").innerHTML = data});
+     console.log(data);
+  else (!response.ok){
+    console.log("Network response was not OK");
   }
-} catch (error){
+   catch (error){
   console.error("There has been a problem with your fetch operation:", error);
 }
 
